c7ec8d92feebee04ebf8b57daa19737031220ddc
Gupta ji <Gupta ji@DESKTOP-GAKKJ0B>
2026-05-24 01:34:32 +0530

    i have provide delete service and toggle service in the app

--- ToDoList/src/main/java/com/todo/service/TaskService.java
+++ ToDoList/src/main/java/com/todo/service/TaskService.java
@@ -25,4 +25,17 @@ public class TaskService {
 		taskRepository.save(task);
 		
 	}
+
+	public void deleteTask(Long id) {
+		taskRepository.deleteById(id);
+		
+	}
+
+	public void toggleTask(Long id) {
+		Task task = taskRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Invalid Task Id"));
+		
+		task.setCompleted(!task.isCompleted());
+		
+		taskRepository.save(task);
+	}
 }
